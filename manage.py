ffeb7a875a6ac484b17fce7ce771e3d6eadaaa25
8cbx <chu849238686@aliyun.com>
2017-03-18 14:20:03 +0800

    update manage.py and .travis.yml

--- manage.py
+++ manage.py
@@ -1,3 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from app import create_app, db
+from app.models import Permission, Role
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+import os
+
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+def make_shell_context():
+    '''
+        make context to the shell
+    :return: dicts
+    '''
+    return dict(app=app, db=db, Permission=Permission, Role=Role)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def test():
+
+    '''
+        run the unit test
+    :return: None
+    '''
+
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    return len(result.failures)
+
+
+if __name__ == '__main__':
+    '''
+        run the judge web
+    '''
+    manager.run()

3d3f870a83ef71deb76400af11b77d6c311dface
8cbx <chu849238686@aliyun.com>
2017-04-10 15:56:42 +0800

    change start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-export CELERY_BROKER_URL="amqp://guest:guest@"+$(RABBITMQ_PORT_5672_TCP_ADDR)+":"+$(RABBITMQ_PORT_5672_TCP_PORT)+"//"
-export CELERY_BACKEND_URL="db+mysql://"+$(MYSQL_USER)+":"+$(MYSQL_PASS)+"@"+$(MYSQL_URL)+":"+$(MYSQL_PORT)+"/celery_backend"
+echo CELERY_BROKER_URL="amqp://guest:guest@$RABBITMQ_PORT_5672_TCP_ADDR:$RABBITMQ_PORT_5672_TCP_PORT//" >> /etc/profile
+
+echo CELERY_BACKEND_URL="db+mysql://$MYSQL_USER:$MYSQL_PASS@$MYSQL_URL:$MYSQL_PORT/celery_backend" >> /etc/profile
+
+source /etc/profile
 
 celery -A celery_work worker --loglevel=info -f ./celery-$HOSTNAME.log -c 2 &
 

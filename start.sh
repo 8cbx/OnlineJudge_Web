92f40d9de75007edfbe9abadb215d7d38dcd1a08
8cbx <chu849238686@aliyun.com>
2017-04-10 17:06:39 +0800

    onsite version, we don't need email part

--- start.sh
+++ start.sh
@@ -6,6 +6,4 @@ echo CELERY_BACKEND_URL="db+mysql://$MYSQL_USER:$MYSQL_PASS@$MYSQL_ADDR:$MYSQL_P
 
 source /etc/profile
 
-celery -A celery_work worker --loglevel=info -f ./celery-$HOSTNAME.log -c 2 &
-
 gunicorn -c guni.conf manage:app

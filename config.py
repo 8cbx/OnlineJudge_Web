6f163e70897adcc9b66644b92c2f283430cc2e88
8cbx <chu849238686@aliyun.com>
2017-05-26 14:18:55 +0800

    Change config file

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Config():
     FLASKY_MAIL_SENDER = os.environ.get('FLASKY_MAIL_SENDER')
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SERVER_NAME = os.environ.get('SERVER_NAME')
-    SEND_EMAIL = True
+    SEND_EMAIL = False
 
     # about sql query and settings
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
